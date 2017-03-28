9fc4d3de326bd638d02c551c31443952efab8d55
ku-zero <ku-zero>
2017-03-29 00:27:30 +0700

    4 golf

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -18,7 +18,7 @@ class SocialAuthController extends Controller
 
     public function callback(SocialAccountService $service)
     {
-        $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
+        $user = $service->createOrGetUser(Socialite::driver('facebook')->stateless()->user());
 
         auth()->login($user);
 
