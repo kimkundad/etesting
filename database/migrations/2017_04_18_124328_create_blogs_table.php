fbf9f09781afcddc699d0c757fca5cd59d1f8cc2
kimkundad <kim.kundad@gmail.com>
2017-04-19 13:28:45 +0700

    songkran golf 4555

--- database/migrations/2017_04_18_124328_create_blogs_table.php
+++ database/migrations/2017_04_18_124328_create_blogs_table.php
@@ -15,7 +15,7 @@ class CreateBlogsTable extends Migration
       Schema::create('blogs', function (Blueprint $table) {
           $table->increments('id');
           $table->string('title_blog');
-          $table->string('detail_blog');
+          $table->text('detail_blog');
           $table->string('image');
           $table->timestamps();
       });
